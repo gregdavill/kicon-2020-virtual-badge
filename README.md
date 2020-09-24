74ee4939afbf8394cadff8f5e5d7cf0ab093cab8
Greg Davill <greg.davill@gmail.com>
2020-09-24 18:03:30 +0930

    Tidy up design

--- README.md
+++ README.md
@@ -2,11 +2,23 @@
 
 ![Render](RENDER/badge.jpg)
 
-Based off the template by [Thomas Flummer](https://gitlab.com/flummer/kicon-2020-virtual-badge)
+Based off the design+template by [Thomas Flummer](https://gitlab.com/flummer/kicon-2020-virtual-badge)
+
+I was inspired by the design Thomas made, and wanted to try and extend the badge with more LEDs. I'm using a similar architecture to the LED panels on the d20 and small LED cube I built, except this only has 1 colour of LED in the Matrix.
+
+## Firmware
+
+I've not coded up firmware for a display like this with a microcontroller before, and I've had trouble with the SAMD21's DMA controller and cyclic descriptors in the past. The display will require constant attention from the SAMD as it is a multiplexed display, I'm hoping that implementing a framebuffer + Binary Coded Modulation will be possible.
+
+## Assembly
+
+The LED matrix is 32x16. Or 512 individual LEDs, these are all 0402 in size, so manual assembly may not be for the casual electronics enthusiast.
+At any rate I'd recommend using a solderpaste stencil and reflow oven.
 
 ## Making your own
 
 If you want to make your own, I have included the gerbers, in case you don't want to install the nightly version.
+The board is 4 layers mostly to assist in routing of the matrix. The cost difference between 2L and 4L at JLC is only $5 on their barebones service.
 
 ## Please share
 
--- create_matrix.py
+++ create_matrix.py
@@ -1,3 +1,8 @@
+# This file is Copyright (c) 2020 Gregory Davill <greg.davill@gmail.com>
+# License: MIT
+
+# Simple script that makes use of the python interface in KiCad to position and route a LED matrix.
+
 import sys
 from pcbnew import *
 pcb = GetBoard()
@@ -5,40 +10,60 @@ pcb = GetBoard()
 from math import sin, acos, cos, asin, pi
 from random import randint
 
+# Determine the PCB width at y-offset (measured in mm from center)
 def width(y):
+    # The PCB edges are formed using a circular tool, with radius around 148mm
     theta = asin(y/148.0)
     c = cos(theta) * 148.0
+
+    # The circular cutout tool is 177mm away from the PCB center line, calculate half_width of the PCB.
     d = (177.0 - c)
+
+    # We want to fit 32 LEDs across, which is 31 equally-spaced gaps. Also add 1 gap to each side for a margin. (/33)
     spacing = (d*2.0)/33.0
+
+    # Some debug info
     #print(c, theta,w,s)
     return (d, spacing)
 
 col_pads = [[] for _ in range(32)]
 
 for y in range(16):
+
+    # Start array 26mm up from center line, use fixed y-spacing of 2mm.
     w,s = width(26 - y*2.0)
 
+    # store an array of via positions along each row, this is a crude way to automatically draw tracks between these.
     row = []
 
     for x in range(32):
+        # Pick out our designators, we expect these to be in order, left to right then top to bottom, D1-D513
         Ref = 'D' + str(1 + x+(y*32))
-    #    print(Ref)
         nPart = pcb.FindModuleByReference(Ref)
-        nPart.SetPosition(wxPoint(FromMM(180 - w +(x+1)*s), FromMM(10+y*2)))  # Update XY
+
+        # Set part X/Y + Rotation
+        nPart.SetPosition(wxPoint(FromMM(180 - w +(x+1)*s), FromMM(10+y*2)))
+        # Yes there is some randomness in the rotation, because I'm evil
         deg = randint(410, 490)/10.0
         nPart.SetOrientationDegrees(deg)
 
+        # Store the pad cathode pad of each LED, this is used to crudely draw tracks later.
         col_pads[x] += [nPart.Pads()[0]]
 
-        # place_via
+        # Place a Via
         deg = -deg - 14.0
         v = VIA(pcb)
         pcb.Add(v)
-        v.SetWidth(600000)
-        v.SetDrill(300000)
+        v.SetWidth(FromMM(0.6))
+        v.SetDrill(FramMM(0.3))
+
+        # Some trig here that is basically just polar->cartesian conversion.
         via_point = wxPoint(FromMM(180 - w +(x+1)*s + 1.2*cos(deg * pi/180.0)), FromMM(10+y*2 + 1.2*sin(deg * pi / 180.0)))
         v.SetPosition(via_point)
 
+        # You'll notice that we are not setting any NETs here, when a track overlaps it will be assiged to a NET automatically by KiCad.
+
+        # Draw track from LED Anode to Via
         t = TRACK(pcb)
         pcb.Add(t)
         t.SetWidth(FromMM(0.25))
@@ -48,8 +73,8 @@ for y in range(16):
         row += [via_point]
         
 
+    # Draw a track segment between every Via in a row, place these on Layer:In_2
     for r in range(len(row)-1):
-        
         t = TRACK(pcb)
         pcb.Add(t)
         t.SetWidth(FromMM(0.25))
@@ -57,6 +82,7 @@ for y in range(16):
         t.SetEnd(row[r+1])
         t.SetLayer(In2_Cu)
 
+# After placing all the LEDs and drawing Vias/Tracks we can draw the vertical tracks connecting cols.
 for c in col_pads:
     for i in range(len(c)-1):
         
@@ -64,6 +90,7 @@ for c in col_pads:
         pcb.Add(t)
         t.SetWidth(FromMM(0.25))
 
+        # We ofset the track to the left by 0.1mm, this is just to add some clearance.
         start = c[i].GetPosition()
         start.x -= FromMM(0.1)
         t.SetStart(start)
@@ -72,6 +99,12 @@ for c in col_pads:
         end.x -= FromMM(0.1)
         t.SetEnd(end)
 
-# KiCad used to let you run scripts with this command, but taht wasn't working for me in Nightly.
-# So I just copied this script to the clipboard, then used `Ctrl + Shift + V` to Paste and run in the internal scriptting console in KiCad.
-# exec(open('leds.py').read())
+
+
+# KiCad used to let you run scripts with the command below, but that wasn't working for me in the Nightly builds, 
+# the scripting console doesn't seem to be operatig in the same working directory as your KiCad project file.
+# `exec(open('leds.py').read())`
+
+# So I just copied this script to the clipboard, then used `Ctrl + Shift + V` to `Paste and Run` inside the internal scripting console in KiCad.
+# Once the script has run you will likely not see any change, there is a bug/quirk in rendering, basically the GUI doesn't think anything has changed, so it won't redraw the screen.
+# I was moving an unrelated via, then saving and restarting pcbnew. This then shows the updated tracks/vias/component placements.
